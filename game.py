1f92529b16695fb6a709a077567909744b331b6b
manandhar01 <manandhar.bibek01@gmail.com>
2020-06-04 09:43:28 +0545

    Collisions detected. :D

--- game.py
+++ game.py
@@ -9,6 +9,8 @@ import random
 # Screen Size
 screenSize = (1024, 768)
 
+# Speed
+speed = 7
 
 # Images
 roadImage = pygame.image.load('images/road.jpg')
@@ -20,19 +22,25 @@ obstacleImages = [
     pygame.image.load('images/oilBarrel.png'),
     ]
 
-FPS = 60
-fpsClock = pygame.time.Clock()
+# FPS = 60
 
 class Game:
     def __init__(self, size):
+        global speed
+        global roadImage
+        global carImage
+        self.fpsClock = pygame.time.Clock()
         self.size = size
-        self.R1 = road.Road(roadImage, [0, 0], 5)
-        self.R2 = road.Road(roadImage, [0, 0 - roadImage.get_rect().height], 5)
-        self.car = car.Car(carImage, [ self.R1.width/2 - carImage.get_rect().width/2, self.R1.height - carImage.get_rect().height - 50 ], 5)
+        self.R1 = road.Road(roadImage, [0, 0], speed)
+        self.R2 = road.Road(roadImage, [0, 0 - roadImage.get_rect().height], speed)
+        self.car = car.Car(carImage, [ self.R1.width/2 - carImage.get_rect().width/2, self.R1.height - carImage.get_rect().height - 50 ], speed)
         self.obstacles = []
+        self.pause = False
 
     def run(self):
-        y = 192
+        global obstacleImages
+        global speed
+        y = 150
         pygame.init
         pygame.display.set_caption('BRAKE FAIL')
         self.screen = pygame.display.set_mode(self.size, 0, 32)
@@ -41,11 +49,13 @@ class Game:
         self.car.image = self.car.image.convert_alpha()
         for images in obstacleImages:
             images = images.convert_alpha()
-        for i in range(4):
-            self.obstacles.append(obstacle.Obstacle(obstacleImages[random.randint(0, 3)],  -y, 6),)
-            y += 192
+        for i in range(3):
+            image = obstacleImages[random.randint(0, 3)]
+            self.obstacles.append(obstacle.Obstacle(image,  [random.randint(0, 1024 - image.get_rect().width), -y], speed))
+            y += 319
         while True:
             self.handleEvents()
+            self.detectCollisions()
             self.updateScreen()
 
     def handleEvents(self):
@@ -53,6 +63,9 @@ class Game:
             if event.type == QUIT:
                 exit()
             elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    self.pause = True
+                    self.paused()
                 if event.key == K_LEFT:
                     self.car.isMovingLeft = True
                 elif event.key == K_RIGHT:
@@ -67,9 +80,9 @@ class Game:
         self.screen.fill((0, 0, 0))
         self.screen.blit(self.R1.image, (self.R1.posx, self.R1.posy))
         self.screen.blit(self.R2.image, (self.R2.posx, self.R2.posy))
-        self.screen.blit(self.car.image, (self.car.posx, self.car.posy))
         for ob in self.obstacles:
             self.screen.blit(ob.image, (ob.posx, ob.posy))
+        self.screen.blit(self.car.image, (self.car.posx, self.car.posy))
         pygame.display.update()
         self.R1.move()
         self.R2.move()
@@ -78,6 +91,33 @@ class Game:
             ob.move()
             if ob.posy > 768:
                 ob.image = obstacleImages[random.randint(0, 3)]
-                ob.posy = -192
+                ob.width = ob.image.get_rect().width
+                ob.posy = -189
                 ob.posx = random.randint(0, 1024 - ob.width)
-        fpsClock.tick(FPS)
+        self.fpsClock.tick(60)
+
+    def paused(self):
+        while self.pause:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    exit()
+                elif event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        self.pause = False
+                elif event.type == KEYUP:
+                    if event.key == K_LEFT:
+                        self.car.isMovingLeft = False
+                    elif event.key == K_RIGHT:
+                        self.car.isMovingRight = False
+
+    def detectCollisions(self):
+        for ob in self.obstacles:
+            if ob.posy + 0.8*ob.height >= self.car.posy and ob.posy + 0.2*ob.height <= self.car.posy + self.car.height:
+                if ob.posx >= self.car.posx:
+                    if self.car.posx + self.car.width > ob.posx + 0.2*ob.width:
+                        self.pause = True
+                        self.paused()
+                else:
+                    if ob.posx + 0.8*ob.width > self.car.posx:
+                        self.pause = True
+                        self.paused()
--- obstacle.py
+++ obstacle.py
@@ -1,12 +1,12 @@
 import random
 
 class Obstacle:
-    def __init__(self, image, posy, speed):
+    def __init__(self, image, pos, speed):
         self.image = image
         self.height = self.image.get_rect().height
         self.width = self.image.get_rect().width
-        self.posx = random.randint(0, 1024 - self.width)
-        self.posy = posy
+        self.posx = pos[0]
+        self.posy = pos[1]
         self.speed = speed
 
     def move(self):
